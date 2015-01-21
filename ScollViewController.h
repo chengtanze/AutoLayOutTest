6778bd2afe64fb1aec4c062ebb5d32ee3cd92786
chengtz <chengtanze@126.com>
2015-01-21 18:01:43 +0800

    scrollview 约束

---
+++ ScollViewController.h
@@ -0,0 +1,18 @@
+//
+//  ScollViewController.h
+//  AutoLayOutTest
+//
+//  Created by wangsl-iMac on 15/1/21.
+//  Copyright (c) 2015年 chengtz-iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScollViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *viewInScoll;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *viewWidth;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *viewHeight;
+
+@end
---
+++ ScollViewController.m
@@ -0,0 +1,45 @@
+//
+//  ScollViewController.m
+//  AutoLayOutTest
+//
+//  Created by wangsl-iMac on 15/1/21.
+//  Copyright (c) 2015年 chengtz-iMac. All rights reserved.
+//
+
+#import "ScollViewController.h"
+
+@interface ScollViewController ()
+
+@end
+
+@implementation ScollViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)updateViewConstraints{
+    [super updateViewConstraints];
+    
+    self.viewWidth.constant = CGRectGetWidth([UIScreen mainScreen].bounds) * 2;
+    
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
